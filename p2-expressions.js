9f7e2a131bb30f66e8e501261b2cfcc966069a55
pablomluna <82621277+pablomluna@users.noreply.github.com>
2021-06-04 23:59:18 -0700

    adding project fiels

---
+++ p2-expressions.js
@@ -0,0 +1,46 @@
+/*
+    CIT 281 Project 2
+    Name: Pablo Manriquez
+*/
+
+// Returns a random number between min (inclusive) and max (exclusive)
+const getRandomInteger = function (min, max) {
+    return Math.floor(Math.random() * (max) + min);
+}
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let result = "";
+
+for (let i = 0; i < getRandomInteger(5, 26); i++) {
+    result += alphabet[getRandomInteger(1,alphabet.length-1)];
+}
+
+console.log(result);
+
+
+// Returns a single, random, lowercase letter
+const getRandomLetter = function() {
+    const abc = "abcdefghijklmnopqrstuvwxyz";
+    for (let i = 0; i = 1; i ++) {
+        return (abc.charAt(Math.floor(Math.random() * abc.length)))
+    }
+}
+
+console.log(getRandomLetter());
+
+// Returns the random length of a string
+const getRandomString = function(minLength, maxLength) {
+    let result = "";
+    for (let i = 0; i < getRandomInteger(10, 20); i ++) {
+        result += " "
+    }
+    return result.length
+}
+console.log(getRandomString());
+
+//Return a string in ascending order
+const getSortedString = function(string) {
+    return (string.split("").sort()).join("")
+}
+
+console.log(getSortedString("xpacd"));
